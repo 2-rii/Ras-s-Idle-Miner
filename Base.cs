e14096e2eb06b5eb7dd94d3100ebd86cccce298d
Ras <muhammadraza12108@gmail.com>
2026-06-04 00:09:30 +0400

    Add files via upload

---
+++ Base.cs
@@ -0,0 +1,125 @@
+//ts place will have the Base and Player details
+
+namespace IdleMiner
+{
+    class Player
+    {
+        public long Money{get; private set;}
+        public int DaysPassed{get; private set;}
+        public string Equipment{get; private set;}
+        public int MiningSpeedModifier{get; private set;}
+        public int ValuablePerc{get; private set;}
+
+        public Player()
+        {
+            Money=0;
+            DaysPassed=0;
+            Equipment="Pickaxe";
+            MiningSpeedModifier=5;
+            ValuablePerc=2;
+        }
+
+        public Player(long money, int daysPassed, string equipment, int miningSpeed, int valuablePerc)
+        {
+            Money=money;
+            DaysPassed=daysPassed;
+            Equipment=equipment;
+            MiningSpeedModifier=miningSpeed;
+            ValuablePerc=valuablePerc;
+        }
+
+        public void increaseMoney(long Increase)
+        {
+            Money+=Increase;
+        }
+
+        public void decreaseMoney(long Decrease)
+        {
+            Money-=Decrease;
+        }
+
+        public void ChangeEquip(string newName, int newSpeed, int newValPerc)
+        {
+            Equipment=newName;
+            MiningSpeedModifier=newSpeed;
+            ValuablePerc=newValPerc;
+        }
+
+        public void PassDay()
+        {
+            DaysPassed++;
+        }
+
+
+
+
+    }
+
+    class Base
+    {
+        public long MaxCapacity{get; private set;}
+        public Dictionary<string,long> CurrentStorage {get; private set;}
+
+        public Base()
+        {
+            MaxCapacity=1000;
+            CurrentStorage= new Dictionary<string, long>
+            {
+                ["Rocks"]= 0,
+                ["Iron Ore"]=0,
+                ["Copper Ore"]=0,
+                ["Gold Ore"]=0,
+                ["Diamond Ore"]=0,
+                ["Viltrum Ore"]=0,
+                ["Yakhadur Ore"]=0,
+                ["Big R Ore"]=0,
+                ["Appalachian Ore"]=0,
+                ["Mink Ore"]=0
+            };
+        }
+
+        public Base(long maxCapacity, Dictionary<string,long> currentStorage)
+        {
+            MaxCapacity=maxCapacity;
+            CurrentStorage=currentStorage;
+        }
+
+        public bool checkifFull()
+        {
+            long total=0;
+            foreach(var(ore, quantity) in CurrentStorage)
+            {
+                total+=quantity;
+            }
+
+            return total >= MaxCapacity; 
+
+        }
+
+        public bool addtoStorage(long rock=0, long ironore=0, long copperore=0, long goldore=0, long diamondore=0, long viltrumore=0, long yakhadurore=0, long bigrore=0, long appalachianore=0, long minkore=0)
+        {
+            if (checkifFull())
+            {
+                return false;
+            }
+            else
+            {
+                //You can go over the storage limit, only cuz im too lazy to implement a system to equally distribute if full
+                CurrentStorage["Rocks"]+=rock;
+                CurrentStorage["Iron Ore"]+=ironore;
+                CurrentStorage["Copper Ore"]+=copperore;
+                CurrentStorage["Gold Ore"]+=goldore;
+                CurrentStorage["Diamond Ore"]+=diamondore;
+                CurrentStorage["Viltrum Ore"]+=viltrumore;
+                CurrentStorage["Yakhadur Ore"]+=yakhadurore;
+                CurrentStorage["Big R Ore"]+=bigrore;
+                CurrentStorage["Appalachian Ore"]+=appalachianore;
+                CurrentStorage["Mink Ore"]+=minkore;
+                return true;
+            }
+        }
+
+
+
+    }
+}
---
+++ Mining.cs
@@ -0,0 +1,95 @@
+namespace IdleMiner
+{
+    class MiningSystem
+    {
+        public Player playerInst {get; private set;}
+        public Base baseInst {get; private set;}
+        private Random randomizer= new Random();
+
+
+        public MiningSystem(Player pI, Base bI)
+        {
+            playerInst=pI;
+            baseInst=bI;
+        }
+
+        public void Mine()
+        {
+            switch (playerInst.Equipment)
+            {
+                case "Pickaxe":
+                    {
+                        long randomRock=randomizer.Next(1,5);
+                        Console.WriteLine($"You have mined {randomRock} rocks with your pickaxe!");
+                        baseInst.addtoStorage(rock: randomRock);
+                        break;
+                    }
+
+                case "Drill":
+                    {
+                        long randomRock=randomizer.Next(1,8);
+                        long randomIronOre=randomizer.Next(1,5);
+                        long randomCopperOre=randomizer.Next(1,3);
+                        Console.WriteLine($"You have mined {randomRock} rocks, {randomIronOre} iron ore and {randomCopperOre} copper ore with your drill!");
+                        baseInst.addtoStorage(rock: randomRock, ironore:randomIronOre, copperore:randomCopperOre);
+                        break; 
+                    }
+
+
+                case "Excavator":
+                    {
+                        long randomRock=randomizer.Next(5,25);
+                        long randomIronOre=randomizer.Next(5,15);
+                        long randomCopperOre=randomizer.Next(5,10);
+                        long randomDiamondOre=randomizer.Next(1,5);
+                        Console.WriteLine($"You have mined {randomRock} rocks, {randomIronOre} iron ore, {randomCopperOre} copper ore and {randomDiamondOre} diamond ore with your excavator!");
+                        baseInst.addtoStorage(rock: randomRock, ironore:randomIronOre, copperore:randomCopperOre, diamondore:randomDiamondOre);
+                        break;
+                    }
+                
+                case "Quarry":
+                    {
+                        long randomRock=randomizer.Next(10,30);
+                        long randomIronOre=randomizer.Next(10,20);
+                        long randomCopperOre=randomizer.Next(10,20);
+                        long randomDiamondOre=randomizer.Next(5,10);
+                        long randomViltrumOre=randomizer.Next(1,3);
+                        Console.WriteLine($"You have mined {randomRock} rocks, {randomIronOre} iron ore, {randomCopperOre} copper ore, {randomDiamondOre} diamond ore and {randomViltrumOre} viltrum ore with your quarry!");
+                        baseInst.addtoStorage(rock: randomRock, ironore:randomIronOre, copperore:randomCopperOre, diamondore:randomDiamondOre, viltrumore:randomViltrumOre);
+                        break;
+                    }
+
+
+            }
+        }
+
+        public void ValuableMine()
+        {
+            int randomPercCompare=randomizer.Next(1,101);
+            int randomIndex=randomizer.Next(0,4);
+            string[] Ores= ["Yakhadur Ore", "Big R Ore", "Appalachian Ore", "Mink Ore"];
+
+            if (playerInst.ValuablePerc >= randomPercCompare)
+            {
+                switch (randomIndex)
+                {
+                    case 0:
+                        baseInst.addtoStorage(yakhadurore:1);
+                        break;
+                    case 1:
+                        baseInst.addtoStorage(bigrore:1);
+                        break;
+                    case 2:
+                        baseInst.addtoStorage(appalachianore:1);
+                        break;
+                    case 3:
+                        baseInst.addtoStorage(minkore:1);
+                        break;
+                }
+                Console.WriteLine($"Woah, you mined one {Ores[randomIndex]} with your {playerInst.Equipment}");
+            }
+        }
+
+
+    }
+}
